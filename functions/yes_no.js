1707d84d51297df18f96c06d25237120254e6aed
Bryan Robinson <bryanlrobinson@gmail.com>
2018-03-24 15:08:17 -0500

    testing how it actually goes

--- functions/yes_no.js
+++ functions/yes_no.js
@@ -1,9 +1,9 @@
 // functions/yes_no.js
 
 exports.handler = function(event, context, callback) {
-    const {yes = 'yes', no = 'no'} = event.queryStringParameters;
-    callback(null, {
-      statusCode: 200,
-      body: Math.floor(Math.random() * 2) ? yes : no,
-    });
-  };
+  const {yes = 'yes', no = 'no'} = event.queryStringParameters;
+  callback(null, {
+    statusCode: 200,
+    body: Math.floor(Math.random() * 2) ? yes : no,
+  });
+};
