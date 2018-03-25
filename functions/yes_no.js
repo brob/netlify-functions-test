8b344958ed7e04ec503081088af6f175b66f46ca
Bryan Robinson <bryanlrobinson@gmail.com>
2018-03-25 15:01:53 -0500

    Adds origin controls to the function

--- functions/yes_no.js
+++ functions/yes_no.js
@@ -1,8 +1,13 @@
 // functions/yes_no.js
 
 exports.handler = function(event, context, callback) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
   const {yes = 'yes', no = 'no'} = event.queryStringParameters;
   callback(null, {
+    headers: {
+      "Access-Control-Allow-Origin" : "*"
+    },
     statusCode: 200,
     body: Math.floor(Math.random() * 2) ? yes : no,
   });
