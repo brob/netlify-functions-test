678ecaacb9a2743ac4c004a4194a2e567009d57d
Bryan Robinson <bryanlrobinson@gmail.com>
2018-03-27 12:32:32 -0500

    Strings the house AFTER randomizing

--- functions/hogwarts.js
+++ functions/hogwarts.js
@@ -1,32 +1,36 @@
 // functions/hogwarts.js
 
-const houses = [
-    {
+const houses = {
+    0: {
         'name': 'Gryffindor',
         'main_color': '174,0,1',
         'secondary_color': '238,186,48'
     },
-    {
+    1: {
         'name': 'Ravenclaw',
         'main_color': '34,47,91',
         'secondary_color': '93,93,93'
     },
-    {
+    2: {
         'name': 'Slytherin',
         'main_color': '42,98,61',
         'secondary_color': '93,93,93'
     },
-    {
+    3: {
         'name': 'Hufflepuff',
         'main_color': '236,185,57',
         'secondary_color': '114,98,85'
     }
-];
+};
 
 
 exports.handler = function(event, context, callback) {
-    var randomHouse = houses[Math.floor(Math.random()*houses.length)];
+    console.log(Object.keys(houses).length);
+    var randomHouse = houses[Math.floor(Math.random()*Object.keys(houses).length)];
+    console.log(randomHouse);
+
     JSON.stringify(randomHouse);
+    console.log(randomHouse);
     callback(null, {
       headers: {
         "Access-Control-Allow-Origin" : "*"
@@ -36,3 +40,6 @@ exports.handler = function(event, context, callback) {
     });
   };
 
+
+
+  exports.handler();
